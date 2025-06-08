a836a12c36effd9f6a63042fdb41b94f39836003
NoOne966 <nidhiskrishna016@outlook.com>
2025-06-08 23:44:30 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,53 @@
+// data.js
+const clusters = [
+  {
+    name: "ALUVA-AIRPORT",
+    description: "Aluva Airport Cluster - Major Hub",
+    machines: [
+      { id: "ALUVA METRO" },
+      { id: "CIAL METRO" },
+      { id: "KL07DE9425"  },
+      { id: "KL07DE9474"  },
+      { id: "KL07DE9435" },
+      { id: "KL07DE9485" },
+      { id: "KL07DE9492" },
+    ],
+    icon: "🚎"
+  },
+  {
+    name: "MG ROAD",
+    description: "MG Road Central Station",
+    machines: [
+      { id: "KL07DE9405" },
+      { id: "KL07DE9420" },
+      { id: "KL07DE9442" },
+    ],
+    icon: "🚐"
+  },
+
+  {
+    name: "INFOPARK",
+    description: "INFOPARK Cluster and Routes",
+    machines: [
+      
+      { id: "KL07DE9436" },
+      { id: "KL07DE9410" },
+      { id: "KL07DE9427" },
+    ],
+    icon: "🚐"
+  },
+
+  {
+    name: "KALAMASSERY",
+    description: "Kalamassery and CUSAT",
+    machines: [
+      
+      { id: "KL07DE9420" },
+      { id: "KL07DE9414" },
+    ],
+    icon: "🚌"
+  },
+  
+];
+
+export default clusters;
